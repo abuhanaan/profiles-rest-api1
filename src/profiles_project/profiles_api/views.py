413f07d8384a2b4c47f9f81539e215e9961ed5bc
abuhanaan <adewunmi99@gmail.com>
2020-11-02 07:20:14 +0100

    Added profile listing and creating feature

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 
+from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-from . import serializers
+from . import serializers, models
 
 # Create your views here.
 
@@ -51,4 +52,59 @@ class HelloApiView(APIView):
         """Deletes an object"""
 
         return Response({'method': 'delete'})
-        
+
+
+class HelloViewSets(viewsets.ViewSet):
+    """Test API ViewSet"""
+
+    
+
+    def list(self, request):
+        """Return a hello message"""
+
+        a_viewset = [
+            'Uses action (list, create, retrieve, update, partial update',
+            'Automatically maps to URLs using Routers',
+            'Provides more functionality with less code'
+        ]
+
+        return Response({'message': 'Hello!', 'a_viewset': a_viewset})
+
+    def create(self, request):
+        """Create new Helo message"""
+
+        serializer = serializers.HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {0}'.format(name)
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        """Handles getting an object by its ID"""
+
+        return Response({'http_method': 'GET'})
+
+    def update(self, request, pk=None):
+        """Handles updating an object"""
+
+        return Response({'http_method': 'PUT'})
+
+    def partial_update(self, request, pk=None):
+        """Handes updating part of an object"""
+
+        return Response({'http_method': 'PATCH'})
+
+    def destroy(self, request, pk=None):
+        """Handles removing an object."""
+
+        return Response({'http_method': 'DELETE'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handels creating, reading, and updating profiles"""
+
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
